c8a8d2eae01eb4b6f87cc09e673a2c0d0d710e5a
César <cesasanc@student.hive.fi>
2025-02-11 15:29:37 +0200

    Added setupSocket

--- Server.cpp
+++ Server.cpp
@@ -6,18 +6,55 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:26:22 by cesasanc          #+#    #+#             */
-/*   Updated: 2025/02/06 13:28:04 by cesasanc         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:27:53 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-Server::Server(int port, const std::string &password) : port(port), password(password)
+Server::Server(int port, const std::string &password) 
+	: port(port), password(password), serverSocket(-1)
 {
+	setupSocket();
 }
 
 Server::~Server()
 {
+	closeServer();
+}
+
+void	Server::setupSocket()
+{
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocket == -1)
+	{
+		std::cerr << "Failed to create socket" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	int flags = fcntl(serverSocket, F_GETFL, 0);
+	if (flags == -1)
+	{
+		std::cerr << "Failed to get socket flags" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	fcntl(serverSocket, F_SETFL, flags | O_NONBLOCK);
+	
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	serverAddress.sin_port = htons(port);
+	
+	if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+	{
+		std::cerr << "Failed to bind socket" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(serverSocket, 10) == -1)
+	{
+		std::cerr << "Failed to listen on socket" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 }
 
 void	Server::run()
--- Server.hpp
+++ Server.hpp
@@ -6,7 +6,7 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:24:45 by cesasanc          #+#    #+#             */
-/*   Updated: 2025/02/06 13:28:45 by cesasanc         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:26:37 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,27 @@
 
 # include <iostream>
 # include <string>
+# include <sys/socket.h>
+# include <fcntl.h>
+# include <netinet/in.h>
 
 class Server
 {
-	private:
-		int			port;
-		std::string	password;
-
 	public:
 		Server(int port, const std::string &password);
 		~Server();
 		
 		void	run();
+
+	private:
+		int				port;
+		std::string		password;
+		int					serverSocket;
+		struct sockaddr_in serverAddress;
+
+		void	setupSocket();
+		void	closeServer();
+
 };
 
 #endif
