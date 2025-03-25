a2993a239a9f49099830f707ef9238d1470e309a
David <dbejar-s@student.hive.fi>
2025-03-26 01:00:53 +0200

    Authenticate and pending commands

--- Commands.cpp
+++ Commands.cpp
@@ -45,3 +45,27 @@ void join(Server *server, int clientFd, const cmd_syntax &parsed) {
     std::string channel = parsed.params[0];
     server->handleJoinCommand(clientFd, channel);
 }
+
+void user(Server *server, int clientFd, const cmd_syntax &parsed) {
+    if (parsed.params.size() < 4) {
+        std::cerr << "Not enough parameters for USER command" << std::endl;
+        return;
+    }
+
+    std::string username = parsed.params[0];
+    std::string hostname = parsed.params[1];
+    std::string servername = parsed.params[2];
+    std::string realname = parsed.message;
+
+    server->handleUserCommand(clientFd, username, hostname, servername, realname);
+}
+
+void pass(Server *server, int clientFd, const cmd_syntax &parsed) {
+    if (parsed.params.empty()) {
+        std::cerr << "No password provided" << std::endl;
+        return;
+    }
+
+    std::string password = parsed.params[0];
+    server->handlePassCommand(clientFd, password);
+}
--- Commands.hpp
+++ Commands.hpp
@@ -8,5 +8,7 @@
 void nick(Server *server, int clientFd, const cmd_syntax &parsed);
 void cap(Server *server, int clientFd, const cmd_syntax &parsed);
 void join(Server *server, int clientFd, const cmd_syntax &parsed);
+void user(Server *server, int clientFd, const cmd_syntax &parsed);
+void pass(Server *server, int clientFd, const cmd_syntax &parsed);
 
 #endif
